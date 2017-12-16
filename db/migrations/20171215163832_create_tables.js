1cc8ba25f4526a7ca801fb8ee23e37337809a10a
nombiezinja <tianyizhang1987@gmail.com>
2017-12-15 16:44:07 -0800

    Adds migrations

---
+++ db/migrations/20171215163832_create_tables.js
@@ -0,0 +1,45 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([knex.schema.createTable('histories', function(table){
+    table.increments('id').primary;
+    table.string('coin_id');
+    table.string('display_name');
+    table.integer('unix_time');
+    table.dateTime('utc_date_time');
+    table.string('market_cap');
+    table.string('price_usd');
+    table.string('price_btc');
+    table.string('open_usd');
+    table.string('open_btc');
+    table.string('close_usd');
+    table.string('close_btc');
+    table.string('volume');
+    table.timestamp('created_at');
+  }),
+  knex.schema.createTable('dailies', function(table) {
+    table.increments('id').primary;
+    table.string('coin_id');
+    table.string('display_name');
+    table.integer('unix_time');
+    table.dateTime('utc_date_time');
+    table.string('market_cap');
+    table.string('price_usd');
+    table.string('price_btc');
+    table.string('open_usd');
+    table.string('open_btc');
+    table.string('close_usd');
+    table.string('close_btc');
+    table.string('volume');
+    table.timestamp('created_at');
+  })
+  ]);
+  
+};
+  
+exports.down = function(knex, Promise) {
+  return Promise.all([
+  knex.schema.dropTable('histories'),
+  knex.schema.dropTable('dailies')
+  ]);
+};
+  
