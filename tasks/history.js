e3fc7f1f1882236b09c67095150489d2e2f85382
nombiezinja <tianyizhang1987@gmail.com>
2017-12-19 13:06:40 -0800

    Adds functional get price function

--- tasks/history.js
+++ tasks/history.js
@@ -3,43 +3,39 @@ const knexConfig = require("../knexfile");
 const knex = require("knex")(knexConfig[ENV]);
 const request = require('request');
 const dbHelper = require.main.require('./helpers/dbHelper')(knex);
-const moment = require('moment')
+const moment = require('moment');
+const fetch = require('node-fetch');
 
 const fetchCrypto = (coinId, coinName) => {
   const startTime = moment(moment.utc().startOf('day')).unix();
-  const endTime = startTime - 86400 * 30;
   const interval = 86400;
+  const endTime = startTime - interval * 30;
 
-  const requestPromises = [];
+  times = [];
 
-  const delay = (ms) => {
-    new Promise(resolve => setTimeout(resolve, ms));
-  }
-
+  i = startTime;
   for (let i = startTime; i >= endTime; i -= interval) {
-    console.log(i, coinId)
-    const url = `https://min-api.cryptocompare.com/data/pricehistorical?fsym=${coinId.toUpperCase()}&tsyms=BTC,USD&ts=${i}`;
-
-    const requestPromise = new Promise(async(resolve, reject) => {
-      request(url, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else {
-          await delay(3000);
-          resolve(body);
-        }
-      });
-    });
-
-    requestPromises.push(requestPromise)
+    times.push(i);
   }
-  async function fetch() {
-    const responses = await Promise.all(requestPromises)
-    console.log(responses)
-    // loop through responses here and save to db 
-  }
-
-  fetch()
+  
+  
+  const getData = async(url) => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      dbHelper.saveHistory(coinId, coinName, i, json).then((id) => {
+        console.log(`Saved db entry ${id}`)
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  console.log(times)
+  j = 0
+  times.forEach((time, j) => { 
+    setTimeout(function (){getData(`https://min-api.cryptocompare.com/data/pricehistorical?fsym=${coinId.toUpperCase()}&tsyms=BTC,USD&ts=${time}`)}, 3000*(j+1)) 
+  });
 
 }
 
