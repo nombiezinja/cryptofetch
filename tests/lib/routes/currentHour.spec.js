a725c5b91b3de72cff0a6e948f168e3923306aab
nombiezinja <tianyizhang1987@gmail.com>
2018-02-17 15:40:39 -0800

    Adds working but incomplete integration test for /currentHour/:coinId

--- tests/lib/routes/currentHour.spec.js
+++ tests/lib/routes/currentHour.spec.js
@@ -1,31 +1,30 @@
-// global.rootRequire = (name) => {
-//   return require(__dirname + '/' + name);
-// }
-
-process.env.NODE_ENV = 'test';
+// Commenting out to run tests in dev environment so as to avoid dealing with databases for now
+// process.env.NODE_ENV = 'test';
 
 const assert = require("chai").assert;
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-// const server = require.main.require('server');
-// const server = rootRequire("./server")
 const server = require("../../../server")
 const should = chai.should();
-
+const expect = require('chai').expect
 
 chai.use(chaiHttp);
 
 describe('Endpoints', () => {
-  // beforeEach((done) => {});
+  // beforeEach((done) => {
+  // });
 
   describe('GET /currentHour/:coin', () => {
     it('it should GET the currentHour history', (done) => {
       chai.request(server)
       .get('/currentHour/eth')
         .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('object');
+          console.log(res.body[0])
+          expect(res).to.have.status(200)
+          expect(res.body).to.be.a('array');
+          expect(res.body.length).to.equal(1);
+          expect(res.body[0]).to.have.all.keys('id','volume_btc');
           done();
         });
     });
