8052812064883ccc09f4df2e3addab8cec12d46c
nombiezinja <tianyizhang1987@gmail.com>
2018-02-16 22:25:13 -0800

    Change all require.root.require back to require(../../ syntax to get things sort of running

--- tests/lib/routes/currentHour.spec.js
+++ tests/lib/routes/currentHour.spec.js
@@ -1,11 +1,18 @@
+// global.rootRequire = (name) => {
+//   return require(__dirname + '/' + name);
+// }
+
 process.env.NODE_ENV = 'test';
 
 const assert = require("chai").assert;
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require.main.require('server');
+// const server = require.main.require('server');
+// const server = rootRequire("./server")
+const server = require("../../../server")
 const should = chai.should();
+
 
 chai.use(chaiHttp);
 
@@ -14,14 +21,13 @@ describe('Endpoints', () => {
 
   describe('GET /currentHour/:coin', () => {
     it('it should GET the currentHour history', (done) => {
-      // console.log(require('path').dirname(require.main.filename))
-      // chai.request(server)
-      // .get('/currentHour/eth')
-      //   .end((err, res) => {
-      //     res.should.have.status(200);
-      //     res.body.should.be.a('object');
-      //     done();
-      //   });
+      chai.request(server)
+      .get('/currentHour/eth')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+        });
     });
   });
 
