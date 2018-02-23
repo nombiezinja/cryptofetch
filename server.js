7c9d37d8bf3fc504f9a2aa9524c2d3600fcfc013
nombiezinja <tianyizhang1987@gmail.com>
2018-02-23 13:38:17 -0800

    Adds ugly query validation logic

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-require('dotenv').config({silent: true});
+require('dotenv').config({
+  silent: true
+});
 
-const ENV = process.env.NODE_ENV; 
+const ENV = process.env.NODE_ENV;
 const port = process.env.PORT || 8080;
 const express = require('express');
 const knexConfig = require('./knexfile');
@@ -23,13 +25,18 @@ const dailiesRoutes = require('./lib/routes/dailies');
 const currentRoutes = require('./lib/routes/current');
 
 const currencies = require('./lib/data/currencies');
+const validParams = require('./lib/data/validParams');
 
 const fs = require('fs')
 const path = require('path')
 
 //writing log to file for now, awaiting further instructions on how logging best handled in aws ecosystem
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
-app.use(morgan('combined', {stream: accessLogStream}))
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
+  flags: 'a'
+})
+app.use(morgan('combined', {
+  stream: accessLogStream
+}))
 
 app.use(knexLogger(knex));
 
@@ -51,20 +58,58 @@ app.get('/test2', (req, res) => {
 });
 
 const checkParams = (params) => {
-  return currencies.filter(currency => (currency.name === params.name));
+  const validParams = currencies.filter(currency => (currency.name === params.name));
+  return validParams.length >>> 0;
 }
 
-const paramsMiddleware = (req, res, next) => {
-  if (checkParams(req.params).length > 0) {
+const checkQuery = async (query) => {
+  // should only have timestamp OR begin + end time, not both or neither
+  const hasBeginAndEndTime = await checkBeginEndTime(query);
+  if (hasBeginAndEndTime && query.timestamp) {
+    return false;
+  }
+  
+  for (value in query) {
+    //all query param values should be timestamps
+    const isTimestamp = await isPositiveInteger(query[value])
+    if (!isTimestamp){
+      return false;
+    } 
+    
+    //only allow certain query params
+    if (validParams.indexOf(value) < 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const checkBeginEndTime = (query) => {
+  if (query.begin_time && query.end_time) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const isPositiveInteger = (n) => {
+  return parseInt(n, 10) >>> 0 === parseFloat(n);
+}
+
+const paramsMiddleware = async (req, res, next) => {
+  const validParams = await checkParams(req.params)
+  const validQuery = await checkQuery(req.query)
+  if (validParams && validQuery) {
     next();
   } else {
     res.sendStatus(400)
   }
 }
 
-app.use('/dailies', dailiesRoutes(paramsMiddleware,Daily));
-app.use('/hourlies', hourliesRoutes(paramsMiddleware,Hourly));
-app.use('/current', currentRoutes(paramsMiddleware,Hourly));
+app.use('/dailies', dailiesRoutes(paramsMiddleware, Daily));
+app.use('/hourlies', hourliesRoutes(paramsMiddleware, Hourly));
+app.use('/current', currentRoutes(paramsMiddleware, Hourly));
 
 server.listen(port, function listening() {
   console.log('Listening on %d', server.address().port);
